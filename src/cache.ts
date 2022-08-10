9062f7747924f281a00a97c3247b3d58ba94e2e5
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2022-08-11 01:26:42 +0200

    Migrate it all to Typescript
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

---
+++ src/cache.ts
@@ -0,0 +1,75 @@
+import exec from '@actions/exec'
+import glob from '@actions/glob'
+import core from '@actions/core'
+import io from '@actions/io'
+
+export async function cacheKey(
+  binTools: string[],
+  deps: string[],
+): Promise<string> {
+  const brewRepositoryResult = await exec.getExecOutput('brew', [
+    '--repository',
+  ])
+  if (brewRepositoryResult.exitCode === 1) {
+    throw "Cannot determine Homebrew's repository path."
+  }
+  const repository = brewRepositoryResult.stdout.trim()
+  const cacheKeyFiles = await Promise.all(
+    binTools.concat(deps).map((value) => {
+      const originalFile = core.toPlatformPath(
+        `${repository}/Library/Taps/homebrew/homebrew-core/Formula/${value}.rb`,
+      )
+      const githubWorkspace = process.env['GITHUB_WORKSPACE'] ?? process.cwd()
+      const destinationFile = core.toPlatformPath(
+        `${githubWorkspace}/brew-cache-${value}.rb`,
+      )
+      io.cp(originalFile, destinationFile)
+      return destinationFile
+    }),
+  )
+  const cacheHash = await glob.hashFiles(cacheKeyFiles.join('\n'))
+  return `brew-formulae-test-${cacheHash}`
+}
+
+export async function cacheFolder(
+  binTools: string[],
+  deps: string[],
+): Promise<string[]> {
+  const toCache: string[] = []
+  const brewCellarResult = await exec.getExecOutput('brew', ['--cellar'])
+  if (brewCellarResult.exitCode === 1) {
+    throw "Cannot determine Homebrew's cellar path."
+  }
+
+  const cellar = brewCellarResult.stdout.trim()
+  const binCacheFolders = binTools.map((value) => {
+    return core.toPlatformPath(`${cellar}/${value}`)
+  })
+  toCache.push(...binCacheFolders)
+
+  const libFolders = deps.map(async (dep) => {
+    return findLibFolder(core.toPlatformPath(`${cellar}/${dep}`))
+  })
+  toCache.push(...(await Promise.all(libFolders)))
+
+  return toCache.filter((result) => result)
+}
+
+async function findLibFolder(root: string): Promise<string> {
+  return exec
+    .getExecOutput('find', [
+      root,
+      '-type',
+      'd',
+      '-maxdepth',
+      '2',
+      '-iname',
+      'lib',
+    ])
+    .then((value) => {
+      return value.stdout
+    })
+    .catch(() => {
+      return ''
+    })
+}
---
+++ src/main.ts
@@ -0,0 +1,97 @@
+import core from '@actions/core'
+import exec from '@actions/exec'
+import cache from '@actions/cache'
+import * as brewCache from './cache'
+
+const debug = core.getBooleanInput('debug')
+
+async function run(): Promise<void> {
+  try {
+    const cacheDeps = core.getBooleanInput('cache-homebrew-deps')
+    const updateResults = await exec.exec('brew', ['update', '--preinstall'])
+    checkCommandFailure(updateResults, 'Cannot update Homebrew.')
+
+    const colimaDepsResult = await exec.getExecOutput('brew', [
+      'deps',
+      'colima',
+    ])
+    checkCommandFailure(colimaDepsResult.exitCode, 'Cannot get Colima deps.')
+    const colimaDeps = colimaDepsResult.stdout.trim().split('\n')
+
+    let cacheHit = false
+    let cacheKey = ''
+    const binTools = ['colima', 'lima', 'qemu', 'docker']
+    if (cacheDeps === true) {
+      const cacheKeyPromise = brewCache
+        .cacheKey(binTools, colimaDeps)
+        .then((key) => {
+          console.log(`Returned key: ${key}`)
+          cacheKey = key
+          console.log(`Updated cacheKey: ${cacheKey}`)
+          return key
+        })
+      const cacheFolderPromise = brewCache.cacheFolder(binTools, colimaDeps)
+      const [folders, key] = await Promise.all([
+        cacheFolderPromise,
+        cacheKeyPromise,
+      ])
+      cacheHit = Boolean(await cache.restoreCache(folders, key))
+      if (debug === true) {
+        console.log('Cache restoration results:')
+        console.log(`\tCache hit: ${cacheHit}`)
+        console.log(`\tCache key: ${cacheKey}`)
+      }
+    }
+
+    if (cacheHit === false) {
+      const installArgs = ['install', '-f', 'colima', 'docker']
+      if (debug === true) {
+        installArgs.splice(1, 0, '-v')
+      }
+      const installResult = await exec.exec('brew', installArgs)
+      checkCommandFailure(
+        installResult,
+        'Cannot install Colima and Docker client.',
+      )
+
+      if (cacheDeps === true) {
+        await brewCache.cacheFolder(binTools, colimaDeps).then((toCache) => {
+          cache.saveCache(toCache, cacheKey)
+          console.log('Cache save results:')
+          console.log(`\tCache folders: ${toCache}`)
+          console.log(`\tCache key: ${cacheKey}`)
+        })
+      }
+    } else {
+      console.log('Homebrew formulae restored from cache. Relinking.')
+      const linkResult = await exec.getExecOutput('brew', ['link', ...binTools])
+      checkCommandFailure(linkResult.exitCode, 'Cannot link Homebrew formulae.')
+    }
+
+    const startResult = await exec.exec('colima', ['start'])
+    checkCommandFailure(startResult, 'Cannot started Colima.')
+
+    exec.getExecOutput('docker', ['version']).then((values) => {
+      checkCommandFailure(values.exitCode, 'Cannot get Docker client version.')
+      core.setOutput('docker-client-version', values.stdout.trim())
+    })
+
+    exec.getExecOutput('colima', ['version']).then((values) => {
+      checkCommandFailure(values.exitCode, 'Cannot get Colima version.')
+      core.setOutput('colima-version', values.stdout.trim())
+    })
+  } catch (error: unknown) {
+    core.setFailed((error as Error).message)
+    if (debug === true) {
+      throw error
+    }
+  }
+}
+
+function checkCommandFailure(result: number, errMsg: string): void {
+  if (result === 1) {
+    throw errMsg
+  }
+}
+
+run()
